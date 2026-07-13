66591c19709cd4704bb9cbcbdbc18cbf5ba39b76
Ani Divakar <anidivakark@gmail.com>
2026-07-13 19:32:43 +0530

    updated test.config.ts

--- test.config.ts
+++ test.config.ts
@@ -1,7 +1,7 @@
 export class TestConfig{
 
-appUrl="https://localhost/opencart/upload/";
-//appUrl = "https://naveenautomationlabs.com/opencart"
+//appUrl="https://localhost/opencart/upload/";
+appUrl = "https://naveenautomationlabs.com/opencart"
 
 
 //valid login credentials- create your own login account
